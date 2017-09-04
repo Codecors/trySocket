4b7dd7e9905583e6236acaac312f7125db4a47e0
Codecors <info@stylehairs.tk>
2017-09-04 23:33:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-server.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 
 const io = socketIO(server);
 
