13932be2ff2a9e0713f582ea078442e14c63c1e9
Codecors <info@stylehairs.tk>
2017-09-04 23:31:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-
+server.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
