5f669c6f29db6af1000b5f16f1381f02162f28af
Codecors <info@stylehairs.tk>
2017-09-04 23:39:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io')(server, { origins: '*:*'});
+const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile('index.html') )
+  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-
 const io = socketIO(server);
-
+io.set('origins', '*:*');
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
