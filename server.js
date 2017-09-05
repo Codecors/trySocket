445ed38754694a17a3e6014bcf5b1aab954bf291
Codecors <info@stylehairs.tk>
2017-09-05 12:50:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 var mysql  =  require("mysql");
 const path = require('path');
+var disposableEmail = require('temporary-email-address-validator');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -15,23 +16,16 @@ const server = express()
 const io = socketIO(server);
 io.set('origins', '*:*');
 
-var connection    =    mysql.createConnection({
 
-      host              :   'pupskee.com',
-      user              :   'pupskee',
-      password          :   'SOW#J@8~xfiUEBK+IAL',
-      database          :   'pupskee',
-    port : 3306
-});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('status added',function(status){
-    connection.connect();
+
     console.log('atleast status on');
       add_status(status,function(res){
         if(res){
-            io.emit('refresh feed',status);
+            io.emit('refresh feed','true');
         } else {
             io.emit('error');
         }
@@ -43,16 +37,9 @@ io.on('connection', (socket) => {
 });
 var add_status = function (status,callback) {
     console.log('status - '+status);
-
-    var sql = "INSERT INTO `status` (`s_text`) VALUES ('"+status+"')";
-    connection.query(sql, function (err, result) {
-            if(!err) {
-              callback(true);
-            }else{
-      console.log(err)};
-    });
- 
-  
+    
+  var result = disposableEmail.validate(status);
+  return result;
 
 }
 //setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
